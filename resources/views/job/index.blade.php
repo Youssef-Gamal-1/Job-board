d1140e7a510c33d9d63f120c4ed97c17200fe0f1
Youssef <yg9552753@gmail.com>
2023-12-03 20:45:49 +0200

    Breadcrumbs navigations

--- resources/views/job/index.blade.php
+++ resources/views/job/index.blade.php
@@ -1,4 +1,9 @@
 <x-layout>
+    <x-bread-crumbs
+        class="mb-4"
+        :links="['Jobs' => route('jobs.index')]"
+    >
+    </x-bread-crumbs>
     @foreach($jobs as $job)
         <x-job-card class="mb-4" :$job> <!-- :$job => :job="$job" -->
             <div>
--- resources/views/job/show.blade.php
+++ resources/views/job/show.blade.php
@@ -1,3 +1,8 @@
 <x-layout>
+    <x-breadcrumbs
+        class="mb-4"
+        :links="['Jobs' => route('jobs.index'),$job->title => '#']"
+    >
+    </x-breadcrumbs>
     <x-job-card :$job />
 </x-layout>
