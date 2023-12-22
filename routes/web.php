cbc277a2389a04c67a5e60f59deb0fd45d2362e0
Youssef <yg9552753@gmail.com>
2023-12-22 15:46:45 +0200

    Job-application establish controller and finish view

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\JobController;
 use \App\Http\Controllers\AuthController;
+use \App\Http\Controllers\JopApplicationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -29,4 +30,8 @@ Route::resource('auth',AuthController::class)
         ->only(['create','store']);
 Route::delete('logout',fn() => to_route('auth.destroy'))->name('logout');
 Route::delete('auth',[AuthController::class,'destroy'])->name('auth.destroy');
+Route::middleware('auth')->group(function(){
+    Route::resource('job.application',JopApplicationController::class)
+            ->only(['create','store']);
+});
 //Route::delete('auth','AuthController@destroy'); the alt for the above
